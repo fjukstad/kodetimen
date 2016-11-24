0cd48527d9847058d65dc814b375dbd25f13c8be
Bjørn Fjukstad <bjorn@cs.uit.no>
2016-11-24 15:27:28 -0500

    using json api not scraping the interwebs
    Thanks @sisomm!

--- kodetimen.go
+++ kodetimen.go
@@ -1,19 +1,42 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
 	"strings"
 
-	"github.com/PuerkitoBio/goquery"
+	"github.com/pkg/errors"
 )
 
 type RegisteredSchool struct {
 	School
 	Registered bool
 }
+type LKKSchool struct {
+	Previous interface{} `json:"_previous"`
+	Locality string      `json:"locality"`
+	Hash     string      `json:"_hash"`
+	Ts       int64       `json:"_ts"`
+	ID       string      `json:"_id"`
+	PostDate string      `json:"post_date"`
+	Updated  int         `json:"_updated"`
+	Level    string      `json:"level"`
+	County   string      `json:"county"`
+	Deleted  bool        `json:"_deleted"`
+	Address  string      `json:"address"`
+	PosLat   string      `json:"pos_lat"`
+	Students int         `json:"students"`
+	School   string      `json:"school"`
+	Year     int         `json:"year"`
+	PosLong  string      `json:"pos_long"`
+}
 
 func getRegisteredSchools() ([]RegisteredSchool, error) {
 	registeredSchools, err := getSchools()
 	if err != nil {
+		fmt.Println(err)
 		return []RegisteredSchool{}, err
 	}
 
@@ -27,7 +50,7 @@ func getRegisteredSchools() ([]RegisteredSchool, error) {
 	for _, school := range allNorwegianSchools {
 		reg := false
 		for _, registered := range registeredSchools {
-			if registered == school.Name {
+			if strings.Contains(school.Name, registered.School) && registered.Locality == school.MunicipalityName {
 				reg = true
 				break
 			}
@@ -37,25 +60,26 @@ func getRegisteredSchools() ([]RegisteredSchool, error) {
 	return schools, nil
 }
 
-func getSchools() ([]string, error) {
-	url := "http://kidsakoder.no/kodetimen/pameldte-skoler-2016/"
-	doc, err := goquery.NewDocument(url)
+func getSchools() ([]LKKSchool, error) {
+	resp, err := http.Get("https://open.sesam.io/api/api/datasets/skoler_med_kodetimen/entities")
+
 	if err != nil {
-		return []string{}, err
+		return []LKKSchool{},
+			errors.Wrap(err, "Could not fetch data from open.sesam.io")
 	}
 
-	var schools []string
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return []LKKSchool{}, errors.Wrap(err, "Could not read http body")
+	}
 
-	// Read list of school from Kidsakoder.no and append to list of schools if
-	// not already in list (some schools are registered more than once).
-	doc.Find(".kodetimen-item").Each(func(i int, s *goquery.Selection) {
-		school := s.Find("span").Text()
-		school = strings.TrimSpace(school)
-		school = strings.Split(school, ",")[0]
-		if !inSlice(school, schools) {
-			schools = append(schools, school)
-		}
-	})
+	var schools []LKKSchool
+	err = json.Unmarshal(body, &schools)
+
+	if err != nil {
+		return []LKKSchool{}, errors.Wrap(err, "Could not unmarshal json from open.sesam.io")
+	}
+
 	return schools, nil
 }
 
